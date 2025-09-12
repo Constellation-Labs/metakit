ffef41b7c555f263db08f0cc912f55741fc907e7
James Aman <jim.a.aman@gmail.com>
2025-09-12 12:10:40 -0500

    chore: update tessellation to 3.4.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object V {
-    val tessellation = "3.4.1"
+    val tessellation = "3.4.5"
     val cats = "2.9.0"
     val catsEffect = "3.4.2"
     val weaver = "0.8.1"
